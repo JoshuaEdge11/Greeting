b50292278d1f8b2b0dcf5b25fe4e4ea921fdd9b5
JoshuaEdge11 <91740729+JoshuaEdge11@users.noreply.github.com>
2021-12-23 20:04:34 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class Main {
-
 public static void main(String[] args)
 
+//This is a simple program that displays Goodmorning if the number entered is lesser than 30, and Goodnight if the nuber entered is greater than 30 
 {
 int num;
 Scanner s= new Scanner(System.in);
